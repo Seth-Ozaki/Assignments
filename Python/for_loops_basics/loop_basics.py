4ed1465353d05af0af10430e232f3684ceeac914
Seth-Ozaki <Sethozaki123@gmail.com>
2024-04-01 16:22:07 -0600

    loop basics

---
+++ Python/for_loops_basics/loop_basics.py
@@ -0,0 +1,26 @@
+for i in range(151):
+    print(i)
+#---------------------
+for j in range(5, 1001):
+    if j%5==0:
+        print(j)
+#---------------------
+for k in range(101):
+    if k%5==0:
+        print("Coding Dojo")
+    else:
+        print(k)
+#---------------------
+sum = 0
+for l in range(500001):
+    if l%2==1:
+        sum+=l
+
+print(sum)
+#---------------------
+for m in range(2018, 0, -4):
+    print(m)
+#---------------------
+for p in range(2, 10):
+    if p%3==0:
+        print(p)
