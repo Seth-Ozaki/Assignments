c349f6afe12cae26240d11f2385da4d0fb58b492
Seth-Ozaki <Sethozaki123@gmail.com>
2024-04-01 21:25:53 -0600

    set up assignment conditions

--- Python/bank_account/account.py
+++ Python/bank_account/account.py
@@ -1,33 +1,37 @@
 class account:
 
     def __init__(self, int_rate = 0.01, balance = 0.00):
-        self.int_rate = int_rate *.01
+        if int_rate == 0.01:
+            self.int_rate = int_rate
+        else:
+            self.int_rate = int_rate *.01
         self.balance = balance
     
     def deposit(self, amount):
         self.balance = self.balance + amount
-        return f"your new balance is ${self.balance}"
+        return self
     def withdraw(self, amount):
         if self.balance < amount:
             self.balance = self.balance - 5
-            return f"insufficient funds: charging a $5 fee"
+            print(f"insufficient funds: charging a $5 fee")
+            return self
         else:
             self.balance = self.balance - amount
-        return f"your new balance is ${self.balance}"
+        return self
+
     def display_account_info(self):
         return f"your current balance is ${self.balance}"
     def yield_interest(self):
         self.balance  = (self.balance * self.int_rate) + self.balance
-        return f"with your {self.int_rate / self.int_rate}% interest added ${self.balance * self.int_rate} worth of interest making your balance ${self.balance}"
-
-account1 = account(1, 100.00)
-
-print(account1.deposit(5))
-print(account1.withdraw(5))
-print(account1.display_account_info())
-print(account1.yield_interest())
+        return self
 
 
-# this checks to see if there is enough money
-# print(account1.withdraw(200))
-# print(account1.display_account_info())
+account1 = account(balance=100)
+account2 = account(1, 100)
+# to check for witdrawl penalty
+account3 = account(balance=100)
+
+print(account1.deposit(5).deposit(5).deposit(5).withdraw(15).yield_interest().display_account_info())
+print(account2.deposit(1000).deposit(2299).withdraw(600).withdraw(200).withdraw(80).withdraw(29).yield_interest().display_account_info())
+# to check for widraw penalty
+print(account3.withdraw(101).display_account_info())
