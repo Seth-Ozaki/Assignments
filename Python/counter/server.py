df8bea3a3f294347ebb8ec7880fa2f35a935c1e5
Seth-Ozaki <Sethozaki123@gmail.com>
2024-04-04 22:22:02 -0600

    adding instruction

--- Python/counter/server.py
+++ Python/counter/server.py
@@ -1,3 +1,20 @@
+
+# Have the root route render a template that displays 
+# the number of times the client has visited this site. 
+# Refresh the page several times to ensure the counter is working.
+
+
+# Add a "/destroy" route that clears the session and redirects
+#  to the root route. Test it.
+
+# NINJA BONUS: Add a +2 button underneath the counter 
+# and a new route that will increment the counter by 2
+
+# NINJA BONUS: Add a Reset button to reset the counter
+
+# SENSEI BONUS: Add a form that allows the user to specify 
+# the increment of the counter and have the counter increment accordingly
+
 from flask import Flask, render_template, request, redirect, session
 app = Flask(__name__)
 app.secret_key='placeholder'
--- Python/html_table/server.py
+++ Python/html_table/server.py
@@ -1,3 +1,11 @@
+# Create a route in which the data above is declared and then 
+# sent to the template engine to be rendered
+
+# Create the template that displays the data in a table
+
+# NINJA BONUS: Use a framework to format the table
+
+
 from flask import Flask, render_template
 app = Flask(__name__)
 
