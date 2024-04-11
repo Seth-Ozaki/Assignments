79d3be15e6edcfd294111ac249ce34a5af1464f1
Seth-Ozaki <Sethozaki123@gmail.com>
2024-04-10 20:13:25 -0600

    users crud assignment

--- Python/user_crud/server.py
+++ Python/user_crud/server.py
@@ -2,10 +2,15 @@ from flask import Flask, render_template, request, redirect, session
 from user import User
 app = Flask(__name__)
 app.secret_key='placeholder key'
-User.clear()
+
 
 @app.route('/')
 def start():
+    User.clear()
+    return render_template('all_user.html')
+
+@app.route('/create')
+def create():
     return render_template('add_user.html')
 
 
@@ -19,6 +24,27 @@ def submit():
     User.submit(request.form)
     return redirect('/all')
 
+@app.route('/user/<int:id>')
+def user(id):
+    user = User.get_one(id)
+    print(user)
+    return render_template('user.html', one_user = user)
+
+
+@app.route('/delete/<int:id>')
+def delete(id):
+    User.delete(id)
+    return redirect('/all')
+
+@app.route('/edit/<int:id>')
+def edit(id):
+    user = User.get_one(id)
+    return render_template('edit.html', one_user = user)
+
+@app.route('/submit_edit/<int:id>', methods=['POST'])
+def submit_edit(id):
+    User.submit_edit(request.form)
+    return redirect(f"/user/{id}")
 
 if __name__=='__main__':
     app.run(debug=True)
--- Python/user_crud/user.py
+++ Python/user_crud/user.py
@@ -1,5 +1,6 @@
 
 from mysqlconnection import connectToMySQL
+from datetime import datetime
 
 class User:
     DB = 'users_crud_mod'
@@ -18,6 +19,7 @@ class User:
         query= "DELETE FROM users WHERE first_name IS NOT null;"
         connectToMySQL(User.DB).query_db(query)
 
+
     
     @classmethod
     def get_all(cls):
@@ -32,8 +34,35 @@ class User:
 
     @classmethod
     def submit(cls, data):
-        query = """INSERT INTO users (first_name,last_name,email)
-                    VALUES (%(first_name)s,%(last_name)s,%(email)s);"""
+        
+        query = """INSERT INTO users (first_name,last_name,email, created_at)
+                    VALUES (%(first_name)s,%(last_name)s,%(email)s,NOW());"""
         result = connectToMySQL(cls.DB).query_db(query,data)
         return result
     
+
+    @classmethod
+    def get_one(cls, id):
+        query = "SELECT * FROM users WHERE id = %(id)s;"
+        results = connectToMySQL('users_crud_mod').query_db(query,{"id":int(id)})
+        print(results)
+        users = []
+        for user in results:
+            users.append( cls(user) )
+            print(users)
+        return users[0]
+    
+    @classmethod
+    def delete(cls, id):
+        query = "DELETE FROM users WHERE id = %(id)s;"
+        result = connectToMySQL(cls.DB).query_db(query,{"id":int(id)})
+        return result
+    
+
+    @classmethod
+    def submit_edit(cls, data):
+        query = """UPDATE users
+                    SET first_name = %(first_name)s,last_name = %(last_name)s,email = %(email)s
+                    WHERE id = %(id)s;"""
+        result = connectToMySQL(cls.DB).query_db(query, data)
+        return result
